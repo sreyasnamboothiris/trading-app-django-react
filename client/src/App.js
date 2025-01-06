2b1a8fa56ead3b7c084bf958b50fb661155a8e88
sreyasnamboothiris <sreyasnamboothiri@gmail.com>
2025-01-06 15:40:23 +0530

    Remove .env file from Git tracking

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './Pages/Login/LoginPage';
-import Otp from './Components/Login/Otp';
 import Profile from './Pages/Profile/Profile';
 import EditProfile from './Components/Profile/ProfileManagement/EditProfile';
 import Admin from './Pages/Admin/Admin';
@@ -17,7 +16,6 @@ function App() {
         <Router>
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path='login/otp/' element={<Otp/>}/>
             <Route path='user/profile/' element={<Profile/>}/>
             <Route path='user/profile/edit' element={<EditProfile/>}/>
             <Route path='admin/' element={<Admin/>}/>
