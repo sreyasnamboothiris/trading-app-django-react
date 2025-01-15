f93abc4ca165d134b939d063a4b2c66e2b12cf18
sreyasnamboothiris <sreyasnamboothiri@gmail.com>
2025-01-15 19:05:10 +0530

    market-data integration working

--- tradeAppServer/market/apps.py
+++ tradeAppServer/market/apps.py
@@ -4,8 +4,3 @@ from django.apps import AppConfig
 
 class MarketConfig(AppConfig):
     name = 'market'
-
-    def ready(self):
-        from .tasks import create_periodic_task
-        # Create periodic task when the app is ready
-        create_periodic_task()
