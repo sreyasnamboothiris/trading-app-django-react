639afceffa4a222d19dbc196cbb9f575bccdd993
sreyasnamboothiris <sreyasnamboothiri@gmail.com>
2025-02-03 18:15:25 +0530

    order ui doing, currently almost done, need more key design parts, (only desing completed)

--- client/src/Pages/Test/Test.jsx
+++ client/src/Pages/Test/Test.jsx
@@ -1,51 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import OrderModal from "./OrderModal";
 
 function Test() {
-  const [priceData, setPriceData] = useState(null);
-  const watchlistId = 3; // Change this ID as needed
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  useEffect(() => {
-    // Connect to the WebSocket server
-    const socket = new WebSocket("ws://localhost:8000/ws/assets/");
+  const handleOpenModal = () => {
+    if (isModalOpen === true){
+      setIsModalOpen(false)
+    } else {
+      setIsModalOpen(true);
+    }
+    
+  };
 
-    // Send watchlist_id after connection opens
-    socket.onopen = () => {
-      socket.send(JSON.stringify({ watchlist_id: watchlistId }));
-    };
-
-    // Handle incoming messages
-    socket.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      console.log("Market Data:", data);
-
-      // Update state with real-time data
-      if (data.initial_prices) {
-        setPriceData(data.initial_prices);
-      } else if (data.updated_prices) {
-        setPriceData(data.updated_prices);
-      }
-    };
-
-    // Handle WebSocket connection errors
-    socket.onerror = (error) => {
-      console.error("WebSocket Error:", error);
-    };
-
-    // Cleanup when component unmounts
-    return () => {
-      socket.close();
-    };
-  }, []);
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
-    <div>
-      <h1>Market Data Stream</h1>
-      <h2>Real-Time Asset Prices:</h2>
-      {priceData ? (
-        <pre>{JSON.stringify(priceData, null, 2)}</pre>
-      ) : (
-        <p>Waiting for real-time data...</p>
-      )}
+    <div className="flex justify-center w-screen">
+      <div className="flex justify-center">
+        Order Management Systemkhsdklfsklfsjkhfsdfksdjhfdkjh
+        <button className="p-2 bg-red-500 m-2" onClick={handleOpenModal}>
+          Buy or Sell
+        </button>
+        {isModalOpen && <OrderModal onClose={handleCloseModal} />}
+      </div>
     </div>
   );
 }
