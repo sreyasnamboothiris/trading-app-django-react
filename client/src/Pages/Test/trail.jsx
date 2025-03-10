14aa52cebef0d00c6cf115194a738486adf2cfe3
sreyasnamboothiris <sreyasnamboothiri@gmail.com>
2025-03-10 17:15:08 +0530

    portfolio ui setup almost completed

--- client/src/Pages/Test/trail.jsx
+++ client/src/Pages/Test/trail.jsx
@@ -1,24 +1,9 @@
-{/* Trailing SL */}
-        {/* <div>
-          <label className="block mb-2">Trailing SL</label>
-          <input
-            type="number"
-            className="w-20 px-3 py-2 bg-transparent border border-[#002F42] rounded-[15px] focus:outline-none"
-          />
-          <div className="flex items-center gap-2 mt-2">
-            <span>In %</span>
-            <button
-              onClick={() => setIsInPrice(!isInPrice)}
-              className={`w-12 h-6 rounded-full p-1 ${
-                isInPrice ? 'bg-blue-600' : 'bg-gray-600'
-              }`}
-            >
-              <div
-                className={`w-4 h-4 bg-white rounded-full transform ${
-                  isInPrice ? 'translate-x-6' : 'translate-x-0'
-                }`}
-              />
-            </button>
-            <span>In Price</span>
-          </div>
-        </div> */}
+import React from 'react'
+
+function trail() {
+  return (
+    <div>trail</div>
+  )
+}
+
+export default trail
