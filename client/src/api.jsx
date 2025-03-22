3a2dfa69d9ce8a4c9b03033612fe3b31545bd3d0
sreyasnamboothiris <sreyasnamboothiri@gmail.com>
2025-03-22 11:53:15 +0530

    ordermodal updated

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'http://127.0.0.1:8000/'
+const WS_URL = 'ws://127.0.0.1:8000/ws/'
 
 const api = axios.create({
     baseURL: API_URL,
@@ -9,5 +10,8 @@ const api = axios.create({
     }
 })
 
+const ws = new WebSocket(WS_URL)
+
+export {ws}
 export default api;
 
